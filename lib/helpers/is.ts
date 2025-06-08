32c03330316ce914c23000b31e5fd5fc58f9043c
hugtyftg <15224924822@163.com>
2025-06-08 13:48:48 +0800

    feat(helper): 实现isPlainObject函数，并修正deepMerge中误用isObject的错误

--- lib/helpers/is.ts
+++ lib/helpers/is.ts
@@ -1,3 +1,5 @@
+import { getPrototypeOf, kindOf } from './index';
+
 // 判断是否为某个类型
 const isTypeof = (type: string) => (thing: any) => typeof thing === type;
 
@@ -18,4 +20,23 @@ export function isArray<T = any>(thing: T[]): thing is T[] {
 
 export function isNil(thing: any): boolean {
   return thing === null || thing === undefined;
+}
+
+export function isPlainObject<T = any>(thing: T): thing is T {
+  // 第一步：类型过滤，排除非对象类型
+  if (kindOf(thing) !== 'object') {
+    return false;
+  }
+  // 第二步：原型链检查
+  const prototype = getPrototypeOf(thing);
+  return (
+    // 情况1：通过 Object.create(null) 创建的无原型对象
+    prototype === null
+    // 情况2：普通对象字面量 {} 或 new Object()
+    || prototype === Object.prototype
+    // 情况3：特殊无原型对象，需同时满足两个条件：toStringTag没被改写、不可迭代
+    || (getPrototypeOf(prototype) === null
+      && !(Symbol.toStringTag in (thing as object))
+      && !(Symbol.iterator in (thing as object)))
+  );
 }

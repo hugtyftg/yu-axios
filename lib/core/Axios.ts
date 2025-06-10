407cbf84d8844b7200a215c607b6d37351e6bf60
hugtyftg <15224924822@163.com>
2025-06-10 12:12:42 +0800

    feat(axios): axios构造器中，根据是否传入data，将method分为两类，各自遍历绑定在Axios原型上，并对postForm等表单方法特殊处理

--- lib/core/Axios.ts
+++ lib/core/Axios.ts
@@ -1,4 +1,5 @@
-import type { AxiosPromise, AxiosRequestConfig, IAxios } from '@/types';
+import type { AxiosPromise, AxiosRequestConfig, IAxios, Method } from '@/types';
+import { transformUrl } from '@/helpers/url';
 import { dispatchRequest } from './dispatchRequest';
 import mergeConfig from './mergeConfig';
 
@@ -6,6 +7,10 @@ export class Axios implements IAxios {
   defaults: AxiosRequestConfig;
   constructor(initConfig: AxiosRequestConfig) {
     this.defaults = initConfig;
+    // 绑定不穿Data的方法，如get
+    this._eachMethodNoData();
+    // 绑定传入Data的方法，如post
+    this._eachMethodWithData();
   }
 
   request(config: AxiosRequestConfig): AxiosPromise {
@@ -15,5 +20,36 @@ export class Axios implements IAxios {
     // });
     config = mergeConfig(this.defaults, config);
     return dispatchRequest(config);
+  }
+
+  getUri(config: AxiosRequestConfig): string {
+    return transformUrl(mergeConfig(this.defaults, config));
+  }
+
+  private _eachMethodNoData() {
+    (['get', 'delete', 'head', 'options'] as Method[]).forEach(method => {
+      (Axios.prototype as Record<string, any>)[method] = (
+        url: string,
+        config: AxiosRequestConfig,
+      ) => this.request(mergeConfig(config, { method, url }));
+    });
+  }
+
+  private _eachMethodWithData() {
+    (['post', 'put', 'patch'] as Method[]).forEach(method => {
+      const genHTTPMethod =
+        (isForm: boolean) => (url: string, data: unknown, config: AxiosRequestConfig) =>
+          this.request(
+            mergeConfig(config, {
+              url,
+              method,
+              data,
+              headers: isForm ? { 'Content-Type': 'multipart/form-data' } : {},
+            }),
+          );
+      // postForm等方法有特殊的请求头Content-Type
+      (Axios.prototype as Record<string, any>)[method] = genHTTPMethod(false);
+      (Axios.prototype as Record<string, any>)[`${method}Form`] = genHTTPMethod(true);
+    });
   }
 }

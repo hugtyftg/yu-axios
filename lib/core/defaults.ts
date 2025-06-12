efd7c8735c438faea6f1a906609532752fbc6008
hugtyftg <15224924822@163.com>
2025-06-12 17:51:47 +0800

    refactor(header helper): 将预处理transformRequestData中，与requestheader相关的工具函数移动到helper/header.ts下

--- lib/core/defaults.ts
+++ lib/core/defaults.ts
@@ -1,6 +1,7 @@
-import type { AxiosRequestConfig, IHeader } from '@/types';
+import type { AxiosRequestConfig } from '@/types';
 import { isPlainObject } from '@/helpers';
 import { isString } from '@/helpers/is';
+import { processRequestHeaders } from '@/helpers/headers';
 
 export const defaults: AxiosRequestConfig = {
   method: 'GET',
@@ -29,34 +30,11 @@ export const defaults: AxiosRequestConfig = {
   },
 };
 
-function processRequestHeaders(headers: IHeader | null | void, data: unknown) {
-  // 格式化Content-Type
-  normalizeHeaderName(headers, 'Content-Type');
-  // 如果有data，则1.转为字符串 2.默认设置Content-Type为application/json
-  if (isPlainObject(data)) {
-    if (headers && !headers['Content-Type']) {
-      headers['Content-Type'] = 'application/json;charset=utf-8';
-    }
-  }
-}
-
 function processRequestData(data: unknown) {
   if (isPlainObject(data)) {
     return JSON.stringify(data);
   }
   return data;
-}
-
-function normalizeHeaderName(headers: IHeader | null | void, normalizedName: string) {
-  if (!headers) {
-    return;
-  }
-  Object.keys(headers).forEach(name => {
-    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
-      headers[normalizedName] = headers[name];
-      delete headers[name];
-    }
-  });
 }
 
 function processResponseData(data: unknown) {
--- lib/helpers/headers.ts
+++ lib/helpers/headers.ts
@@ -1,5 +1,5 @@
 import { IHeader, Method } from '@/types';
-import { deepMerge } from '.';
+import { deepMerge, isPlainObject } from '.';
 
 export function flattenHeaders(
   headers: IHeader | null | undefined,
@@ -17,4 +17,29 @@ export function flattenHeaders(
     delete headers![method];
   });
   return headers;
+}
+
+// 工具函数：预处理请求头
+export function processRequestHeaders(headers: IHeader | null | void, data: unknown) {
+  // 格式化Content-Type
+  normalizeHeaderName(headers, 'Content-Type');
+  // 如果有data，则1.转为字符串 2.默认设置Content-Type为application/json
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=utf-8';
+    }
+  }
+}
+
+// 工具函数：规范化header key
+export function normalizeHeaderName(headers: IHeader | null | void, normalizedName: string) {
+  if (!headers) {
+    return;
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = headers[name];
+      delete headers[name];
+    }
+  });
 }

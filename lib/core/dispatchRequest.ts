70ffc5a6f49e1920e71d1905fa2a38943f65d00e
hugtyftg <15224924822@163.com>
2025-06-12 17:32:25 +0800

    feat(transformdata): 发送请求前预处理header和data，接受响应后预处理response

--- lib/core/dispatchRequest.ts
+++ lib/core/dispatchRequest.ts
@@ -1,19 +1,51 @@
-import type { Adapter, AxiosPromise, AxiosRequestConfig } from '@/types';
+import type {
+  Adapter,
+  AxiosPromise,
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosTransformer,
+} from '@/types';
 import adapters from '@/adapter';
 import { flattenHeaders } from '@/helpers/headers';
+import { isArray } from '@/helpers/is';
 import { transformUrl } from '@/helpers/url';
 import { defaults } from './defaults';
+import transformData from './transformData';
 
 export function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
   // 发送前检查是否已经被取消
   config.cancelToken?.throwIfRequested();
   processConfig(config);
   const adapter = adapters.getAdapter(config?.adapter || (defaults.adapter as Adapter));
-  return adapter(config);
+  return adapter(config).then(response => transformResponseData(response));
 }
 
 // 对url、headers、data预处理
 function processConfig(config: AxiosRequestConfig) {
   config.url = transformUrl(config);
-  config.headers = flattenHeaders(config, config.method ?? 'GET');
+  config.data = transformRequestData(config);
+  config.data = config.headers = flattenHeaders(config, config.method ?? 'GET');
+}
+
+// transformer: 发送请求前对headers和data进行预处理
+function transformRequestData(config: AxiosRequestConfig) {
+  const data = transformData.call(
+    config,
+    isArray(config.transformRequest)
+      ? (config.transformRequest as AxiosTransformer[])
+      : [config.transformRequest],
+  );
+  return data;
+}
+
+// transformer: 接收响应数据进行预处理
+function transformResponseData(response: AxiosResponse) {
+  response.data = transformData.call(
+    response,
+    isArray(response.config.transformResponse)
+      ? (response.config.transformResponse as AxiosTransformer[])
+      : [response.config.transformResponse],
+    response,
+  );
+  return response;
 }

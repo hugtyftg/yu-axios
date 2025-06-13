e4f045b30c2c79f2a58274bebe71be50d88fc5c9
hugtyftg <15224924822@163.com>
2025-06-13 17:07:12 +0800

    feat(transformerrordata): 响应出现error后的处理

--- lib/cancel/CancelError.ts
+++ lib/cancel/CancelError.ts
@@ -3,7 +3,7 @@ import { AxiosRequestConfig, CancelError as ICancelError } from '@/types';
 
 export default class CancelError extends AxiosError implements ICancelError {
   __CANCEL__: boolean;
-  constructor(message: string | undefined, config: AxiosRequestConfig, request: XMLHttpRequest) {
+  constructor(message: string | undefined, config: AxiosRequestConfig, request?: XMLHttpRequest) {
     super(message ?? 'Cancel', ErrorCodes.ERR_CANCELED.value, config, request);
     this.__CANCEL__ = true;
   }
